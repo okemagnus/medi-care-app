aff553ae7c0601179c779ad15c47590c1cb11820
Magnus Okechi <okemagnus@gmail.com>
2025-11-07 11:04:32 +0100

    First commit - setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to Medicare Dashboard</h1>
+      <p>Your healthcare inventory and drug management system</p>
+    </div>
+  );
+}
+
+export default App;
